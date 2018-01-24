6a9cfdcabbe6e53e125bbde363bc2dd31322511e
adwas <adam.wasniewski@gmail.com>
2018-01-24 20:15:34 +0100

    Added unit tests for exercises

---
+++ exercises/Readme.txt
@@ -0,0 +1,10 @@
+
+To run the tests execute:
+python3 -m pytest
+
+To see more detailed execution add -v 
+python3 -m pytest -v
+
+To narrow execution use keyword switch -k:
+ python3 -m pytest -v -k TestTemperatureCase
+
---
+++ exercises/tests/a001_temp_test.py
@@ -0,0 +1,39 @@
+'''
+Create class Temerature that holds a temperature in Fahrenheit
+and provide methods to convert to Celsius and Kelvin.
+Insert it in monitor package.
+
+T(°C) = (T(°F) - 32) × 5/9
+T(K) = (T(°F) + 459.67)× 5/9
+T(K) = T(°C) + 273.15
+
+Conversion results should be rounded to two digits.
+
+'''
+import unittest
+from src import monitor
+
+class TestTemperatureCase(unittest.TestCase):
+
+    def test_fahrenheit_temperature_monitor(self):
+
+        temp = monitor.Temperature(-40)
+
+        self.assertEqual(temp.as_fahrenheit(), -40.0,
+                         ' Incorrect fahrenheit value')
+        self.assertEqual(temp.as_celsius(), -40.0,
+                         ' Incorrect celsius value')
+        self.assertEqual(temp.as_kelvin(), 233.15,
+                         ' Incorrect kelvin value')
+
+
+    def test_float_fahrenheit_temperature_monitor(self):
+
+        temp = monitor.Temperature(-459.673456)
+
+        self.assertEqual(temp.as_fahrenheit(), -459.67,
+                         ' Incorrect fahrenheit value')
+        self.assertEqual(temp.as_celsius(), -273.15,
+                         ' Incorrect celsius value')
+        self.assertEqual(temp.as_kelvin(), 0,
+                         ' Incorrect kelvin value')
